255589d95bda715f384bcc4daa313343d28a53cd
Andrii Leskiv <leshost@gmail.com>
2020-08-19 00:23:09 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todosRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(todosRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://rnet:yoyo8615@cluster0.c5mlq.mongodb.net/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
